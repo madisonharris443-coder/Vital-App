da22988a59cb78aea04d14784798ace92cce6f25
madisonharris443-coder <madisonharris443@gmail.com>
2026-07-28 22:23:27 -0400

    Update server.js

--- server.js
+++ server.js
@@ -831,4 +831,6 @@ app.get("/product-search", async function(req, res) {
   }
 });
 
-app.listen(3000, function() { console.log("VITAL running on port 3000"); });
+app.listen(process.env.PORT || 3000, function() {
+  console.log("VITAL running on port " + (process.env.PORT || 3000));
+});
